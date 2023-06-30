807343601385f4d4e48b12af44180ae74ec80c45
deepennsnow <138068674+deepennsnow@users.noreply.github.com>
2023-06-30 10:56:39 -0600

    Update Fruityvice_app.py

--- Fruityvice_app.py
+++ Fruityvice_app.py
@@ -1,1 +1,5 @@
+#New Section to display fruityvice api response
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
